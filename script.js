a277f9a1073a8a5e55af13ab02f6781f0fcfc77b
Emin <eminkhidirov98@gmail.com>
2023-01-02 09:59:36 +0300

    emin

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+"use strict";
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// if (1) {
+//   console.log("Ok!");  
+// } else {
+//   console.log("Error");
+// }
 
-};
+// const num = 50;
 
-   const a = prompt("Один из последних просмотренных фильмов?", ""),
-         b = prompt("На сколько щцените его?",""), 
-         c = prompt("Один из последних просмотренных фильмов?", ""),
-         d = prompt("На сколько щцените его?","");
+// if (num < 49) {
+//    console.log("Error");
+// } else if (num > 100) {
+//    console.log("Много"); 
+// } else {
+//    console.log("Ok!");
+// }
+// (num === 50) ? console.log("Ok!") : console.log("Error");
 
-         personalMovieDB.movies[a] = b;
-         personalMovieDB.movies[a] = d;
+// const num = "50";
 
-         console.log(personalMovieDB);
+// switch (num) {
+//     case "49":
+//         console.log("Неверно");
+//         break;
+//     case "100":  
+//         console.log("Неверно"); 
+//        break;
+//     case "50":  
+//        console.log("В точку");
+//        break;   
+//     default:  
+//        console.log("Не в этот раз");  
+//        break;    
+// }
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//     console.log("Я сыт!");
+// }
+// console.log((hamburger && fries));
+const hamburger = 3;
+const fries = 3;
+const cola = 0;
+const nuggets = 2;
+
+if (hamburger === 3 && cola ===2 || fries === 3 && nuggets) {
+  console.log("Все довольны");
+} else {
+  console.log("Мы уходим") 
+}
+console.log(hamburger === 3 && cola ===2 || fries === 3 && nuggets);
+
+// let johnReport, alexReport, samReport, mariaReport = "done";
+
+// console.log(johnReport || alexReport || samReport || mariaReport);
