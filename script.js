5f09936f240d441d3f5173ba7c019ccea2264c5b
Emin <eminkhidirov98@gmail.com>
2023-01-04 18:07:14 +0300

    рудщщщщ

--- script.js
+++ script.js
@@ -78,14 +78,48 @@
 //    console.log("Я сыт")
 // }
 
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries)
+
+// if (hamburger === 3 && fries) {
+//    console.log("vse siti");
+// } else {
+//    console.log("mi uxodim");
+// }
+
 const hamburger = 3;
-const fries = 1;
+const fries = 3;
 const cola = 0;
+const nuggets = 3;
 
-console.log(hamburger === 3 && cola && fries)
-
-if (hamburger === 3 && fries) {
-   console.log("vse siti");
-} else {
-   console.log("mi uxodim");
-}
+   if (hamburger === 3 &&  cola === 2 || fries === 3 && nuggets ) {
+      console.log("Все довольны")
+   } else {
+      console.log("Мы уходим")
+   } 
+   console.log(hamburger === 3 &&  cola === 2 || fries === 3 && nuggets);
+console .log(!false);
+console.log( NaN || 2 || undefined )
+console.log( NaN && 2 && undefined );
+console.log( NaN && 2 && undefined );
+console.log( !1 && 2 || !3 );
+console.log( 25 || null && !3 );
+console.log( 5 === 5 && 3 > 1 || 5);
+console.log( 5 === 5 && 3 > 1 || 5);
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
+   let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+ 
+if (hamburger || cola || fries === 3 || nuggets) {
+   console.log('Done!')
